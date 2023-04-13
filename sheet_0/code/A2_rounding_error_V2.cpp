24d7115828e3b2afa5609b7ecd72ce1539331904
SamGTex <samuel.haefs@tu-dortmund.de>
2023-04-13 10:52:01 +0200

    adjust typo a) -> c)

--- sheet_0/code/A2_rounding_error_V2.cpp
+++ sheet_0/code/A2_rounding_error_V2.cpp
@@ -52,7 +52,7 @@ int main()
     cout << "Relative error between the nummerically unstable function f and stable function g:" << endl;
     cout << "a) x = " << x_high << ", f(x) = " << f1(x_high) << " , g(x) = "<< g1(x_high) << ", rel. error: " << rel_error(g1(x_high), f1(x_high)) << endl;
     cout << "b) x = " << x_low << ", f(x) = " << f2(x_low) << " , g(x) = "<< g2(x_low) << ", rel. error: " << rel_error(g2(x_low), f2(x_low)) << endl;
-    cout << "a) x = " << x_high << ", delta = " << delta << ", f(x) = " << f3(x_high, delta) << " , g(x) = "<< g3(x_high, delta) << ", rel. error: " << rel_error(g3(x_high, delta), f3(x_high, delta)) << endl;
+    cout << "c) x = " << x_high << ", delta = " << delta << ", f(x) = " << f3(x_high, delta) << " , g(x) = "<< g3(x_high, delta) << ", rel. error: " << rel_error(g3(x_high, delta), f3(x_high, delta)) << endl;
 
     return 0;
 }
