dd8c57e64df71e51e574ceb8f05dbfae98757356
Slin1999 <selin88.sz@gmail.com>
2023-04-16 21:35:14 +0200

    Blatt1 A1 zwischenstand

--- sheet_1/code/A1.cpp
+++ sheet_1/code/A1.cpp
@@ -2,11 +2,76 @@
 #include <functional>
 #include <iostream>
 #include <math.h>
+using namespace std;
 
-double f2(double x) {
-    return x * sin(1 / x);
+float df_zwei(std::function<float(float)> f, float x, float h) {
+    return (f(x+h) - f(x-h))/(2*h);
 }
+
+
+float df2_zwei(std::function<float(float)> f, float x, float h) {
+    return (df_zwei(f, x+h, 0.01) - df_zwei(f, x-h, 0.01))/(2*h);
+}
+
+float df_vier(std::function<float(float)> f, float x, float h) {
+    return (-f(x+2*h) + 8*f(x+h) - 8*f(x-h) + f(x-2*h))/(12*h);
+}
+
+float f1(float x) {
+    return sin(x);
+}
+
+
+float f2(float x) {
+    if (x >= 0) {
+        return 2 * floor(x/M_PI) - cos( fmod(x, M_PI)) +1;       
+    } else {
+        return 2 * floor(x/M_PI) + cos( fmod(x, M_PI)) +1;
+    }
+}
+
 
 int main() {
-    return 0;
+//a)
+ofstream File1("build/A1_a_h.csv");
+    for (float h = 0.000000001; h <= 1.0; h *=10) {
+        File1 << h << "," << df_zwei(f1, 1.5, h) << endl;
+    }
+    File1.close();
+
+ofstream File2("build/A1_a_x.csv");
+    for (float x = -M_PI; x <= M_PI; x +=0.1) {
+        File2 << x << "," << df_zwei(f1, x, 0.01) << endl;
+    }
+    File2.close();
+
+//b)
+ofstream File3("build/A1_b_h.csv");
+    for (float h =  0.000000001; h <= 1.0; h *=10) {
+        File3 << h << "," << df2_zwei(f1, 1.5, h) << endl;
+    }
+    File3.close();
+
+ofstream File4("build/A1_b_x.csv");
+    for (float x = -M_PI; x <= M_PI; x +=0.1) {
+        File4 << x << "," << df2_zwei(f1, x, 0.01) << endl;
+    }
+    File4.close();
+
+//c)
+ofstream File5("build/A1_c_h.csv");
+    for (float h = 0.000000001; h <= 1.0; h *=10) {
+        File5 << h << "," << df_vier(f1, 1.5, h) << endl;
+    }
+    File5.close();
+
+ofstream File6("build/A1_c_x.csv");
+    for (float x = -M_PI; x <= M_PI; x +=0.1) {
+        File6 << x << "," << df_vier(f1, x, 0.01) << endl;
+    }
+    File6.close();
+
+
+return 0;
 }
+
--- sheet_1/code/plot.py
+++ sheet_1/code/plot.py
@@ -1,14 +1,85 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
 
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$x$')
-plt.ylabel(r'$y$')
-plt.legend(loc='best')
+#A1
+#a)################################################################################################
+ha, dfa = np.genfromtxt("build/A1_a_h.csv", unpack=True, delimiter= ",")
+plt.plot(ha, dfa, "x")
+plt.plot(ha[np.where(np.abs(dfa-np.cos(1.5))== np.amin(np.abs(dfa-np.cos(1.5))))[0][0]], 
+         dfa[np.where(np.abs(dfa-np.cos(1.5))== np.amin(np.abs(dfa-np.cos(1.5))))[0][0]], "x",
+         label=r"${|f'_{zweipunkt}(1.5,h)}-f'(1.5)|_{min}}$")
+plt.xscale("log")
+plt.xlabel(r'$h$')
+plt.ylabel(r"$f'_{zweipunkt}(x)$")
+plt.title(r"$f=\sin(x), x=1.5$")
+plt.legend(loc="best")
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/plot.pdf')
+plt.clf()
+
+######
+
+xa, dfax = np.genfromtxt("build/A1_a_x.csv", unpack=True, delimiter= ",")
+
+plt.plot(xa, np.abs(dfax-np.cos(xa)))
+plt.xlabel(r'$x$')
+plt.ylabel(r"$|f'_{zweipunkt}-f'_{analytisch}|$")
+plt.title(r"$f=\sin(x), h=0.01$")
+
+plt.savefig('build/A1_a_x.pdf')
+plt.clf()
+
+#b)#########################################################################################
+hb, dfb = np.genfromtxt("build/A1_b_h.csv", unpack=True, delimiter= ",")
+plt.plot(hb, dfb, "x")
+plt.plot(hb[np.where(np.abs(dfb+np.sin(1.5))== np.amin(np.abs(dfb+np.sin(1.5))))[0][0]], 
+         dfb[np.where(np.abs(dfb+np.sin(1.5))== np.amin(np.abs(dfb+np.sin(1.5))))[0][0]], "x",
+         label=r"${|f''_{zweipunkt}(1.5,h)}-f''(1.5)|_{min}}$")
+plt.xscale("log")
+plt.xlabel(r'$h$')
+plt.ylabel(r"$f''_{zweipunkt}$")
+plt.title(r"$f''=-\sin(x), x=1.5, h_{f'}=0.01$")
+plt.legend(loc="best")
+
+plt.savefig('build/A1_b_h.pdf')
+plt.clf()
+#print(hb[np.where(np.abs(dfb+np.sin(1.5))== np.amin(np.abs(dfb+np.sin(1.5))))[0][0]])
+#print(-np.sin(1.5))
+
+xb, dfbx = np.genfromtxt("build/A1_b_x.csv", unpack=True, delimiter= ",")
+
+plt.plot(xb, np.abs(dfbx + np.sin(xb)))
+plt.xlabel(r'$x$')
+plt.ylabel(r"$|f''_{zweipunkt}-f''_{analytisch}|$")
+plt.title(r"$f=\sin(x), h=0.01, h_{f'}=0.01$")
+
+plt.savefig('build/A1_b_x.pdf')
+plt.clf()
+
+#c)######################################################################
+hc, dfc = np.genfromtxt("build/A1_c_h.csv", unpack=True, delimiter= ",")
+plt.plot(hc, dfc, "x")
+plt.plot(hc[np.where(np.abs(dfc-np.cos(1.5))== np.amin(np.abs(dfc-np.cos(1.5))))[0][0]], 
+         dfc[np.where(np.abs(dfc-np.cos(1.5))== np.amin(np.abs(dfc-np.cos(1.5))))[0][0]], "x",
+         label=r"${|f'_{vierpunt}(1.5,h)}-f'(1.5)|_{min}}$")
+plt.xscale("log")
+plt.xlabel(r'$h$')
+plt.ylabel(r"$f'_{vierpunkt}(x)$")
+plt.title(r"$f=\sin(x), x=1.5$")
+plt.legend(loc="best")
+
+plt.savefig('build/A1_c_h.pdf')
+plt.clf()
+
+#print(hc[np.where(np.abs(dfc-np.cos(1.5))== np.amin(np.abs(dfc-np.cos(1.5))))[0][0]])
+
+xc, dfcx = np.genfromtxt("build/A1_c_x.csv", unpack=True, delimiter= ",")
+
+plt.plot(xc, np.abs(dfcx-np.cos(xc)))
+plt.xlabel(r'$x$')
+plt.ylabel(r"$|f'_{vierpunkt}-f'_{analytisch}|$")
+plt.title(r"$f=\sin(x), h=0.01$")
+
+plt.savefig('build/A1_c_x.pdf')
+plt.clf()
