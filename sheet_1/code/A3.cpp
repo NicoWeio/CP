925c0c1f9cea7051ffa4335122df8b24f8614cc6
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2023-04-21 15:33:52 +0200

    B1A3: generalisierte Integrations-Wrapper-Funktion

--- sheet_1/code/A3.cpp
+++ sheet_1/code/A3.cpp
@@ -12,11 +12,9 @@ double f2(double x){
     return x*sin(1/x);
 }
 
-// Ich habe es leider nicht geschafft die drei unten stehenden Funktionen zu einer zusammenzufassen mit einem
-// zusätzlichen Argument: Zeiger auf Funktion (trapezregel, mittelpunkt, simpson)
-// da diese funktionen selbst bereits als argument einen zeiger auf eine funktion besitzen
+typedef double (*integrator_fn)(double (*f)(double), double a, double b, double h);
 
-void integrate_trapez(double (*f)(double), double a, double b, std::string path_output, double eps){
+void integrate_general(integrator_fn integrator, double (*f)(double), double a, double b, std::string path_output, double eps){
     std::ofstream myfile(path_output);
 
     double err = 1.0;
@@ -24,63 +22,18 @@ void integrate_trapez(double (*f)(double), double a, double b, std::string path_
 
     double I;
     double I_old;
-    
-    I = trapezregel(f1, a, b, h);
+
+    I = integrator(f1, a, b, h);
     while(err>eps){
         myfile << I << std::endl;
         h = h/2;
         I_old = I;
-        I = trapezregel(f1, a, b, h);
+        I = integrator(f1, a, b, h);
 
         err = abs((I-I_old)/I_old);
     }
     myfile.close();
 }
-
-void integrate_mittelpunkt(double (*f)(double), double a, double b, std::string path_output, double eps){
-    std::ofstream myfile(path_output);
-
-    double err = 1.0;
-    double h = abs(b-a)/2;
-
-
-    double I;
-    double I_old;
-    
-    I = mittelpunktsregel(f1, a, b, h);
-    while(err>eps){
-        myfile << I << std::endl;
-        h = h/2;
-        I_old = I;
-        I = mittelpunktsregel(f1, a, b, h);
-
-        err = abs((I-I_old)/I_old);
-    }
-    myfile.close();
-}
-
-void integrate_simpson(double (*f)(double), double a, double b, std::string path_output, double eps){
-    std::ofstream myfile(path_output);
-
-    double err = 1.0;
-    double h = abs(b-a)/2;
-
-
-    double I;
-    double I_old;
-    
-    I = simpsonregel(f1, a, b, h);
-    while(err>eps){
-        myfile << I << std::endl;
-        h = h/2;
-        I_old = I;
-        I = simpsonregel(f1, a, b, h);
-
-        err = abs((I-I_old)/I_old);
-    }
-    myfile.close();
-}
-
 
 int main() {
     const double eps = pow(10,-4);
@@ -88,15 +41,14 @@ int main() {
     const double I2_exact =  0.378530;
 
     // a)
-    integrate_trapez(f1, 1, 100, "build/I1_trapez.csv", eps);
-    integrate_mittelpunkt(f1, 1, 100, "build/I1_mittelpunkt.csv", eps);
-    integrate_simpson(f1, 1, 100, "build/I1_simpson.csv", eps);
-
+    integrate_general(trapezregel, f1, 1, 100, "build/I1_trapez.csv", eps);
+    integrate_general(mittelpunktsregel, f1, 1, 100, "build/I1_mittelpunkt.csv", eps);
+    integrate_general(simpsonregel, f1, 1, 100, "build/I1_simpson.csv", eps);
 
     // b)
-    //integrate_trapez(f2, 0, 1, "build/I2_trapez.csv", eps);
-    //integrate_mittelpunkt(f2, 0, 1, "build/I2_mittelpunkt.csv", eps);
-    //integrate_simpson(f2, 0, 1, "build/I2_simpson.csv", eps);
+    // integrate_general(trapezregel, f2, 0, 1, "build/I2_trapez.csv", eps);
+    // integrate_general(mittelpunktsregel, f2, 0, 1, "build/I2_mittelpunkt.csv", eps);
+    // integrate_general(simpsonregel, f2, 0, 1, "build/I2_simpson.csv", eps);
 
     return 0;
 }
