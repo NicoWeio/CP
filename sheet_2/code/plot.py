b0034b439316c3d95ce8e7b52e31b835b884b12e
Slin1999 <selin88.sz@gmail.com>
2023-04-26 19:58:23 +0200

    Sheet 2 A1 zwischenstand

--- sheet_2/code/plot.py
+++ sheet_2/code/plot.py
@@ -1,10 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+S, r, z, phi =np.genfromtxt("build/A1.csv", unpack=True, delimiter=",")
 
-plt.plot(x, y, label='Kurve')
+plt.plot(r, z, label='Kurve')
 plt.xlabel(r'$x$')
 plt.ylabel(r'$y$')
 plt.legend(loc='best')
@@ -12,3 +11,5 @@ plt.legend(loc='best')
 # in matplotlibrc leider (noch) nicht möglich
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/plot.pdf')
+
+
