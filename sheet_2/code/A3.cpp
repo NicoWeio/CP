0d99aa23288b4c801c30f3e8979a0c69197f3d5d
SamGTex <samuel.haefs@tu-dortmund.de>
2023-04-26 21:27:48 +0200

    implemented the euler algorithm -> works implemented the verlet algorithm -> dont know what is the initial value for r_n-1

---
+++ sheet_2/code/A3.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <vector>
+#include "vector_functions.h"
+#include <fstream>
+#include <cstdlib> //for exit(1) to catch errors
+
+// calc acceleration vector a
+std::vector<double> get_a(const std::vector<double>& r1, const std::vector<double>& r2, const double M){
+    // a = F(r1, r2)/m
+    std::vector<double> r_dif = vec_sub(r1, r2);
+    
+    //check if r1-r2 == 0
+    if(vec_abs(r_dif)==0){
+        std::cerr << "Division by zero: r1 and r2 must be different!";
+        std::exit(1);
+    }
+
+    double factor = -M / pow(vec_abs(r_dif), 3);
+    return vec_scalar(r_dif, factor);
+}
+
+// calc velocity vector v
+std::vector<double> get_v(const std::vector<double>& v_old, const std::vector<double>& a, const double h){
+    // v + a*h
+    return vec_add(v_old, vec_scalar(a, h));
+}
+
+// calc space vector r
+std::vector<double> get_r(const std::vector<double>& r_old, const std::vector<double>& v, const double h){
+    return vec_add(r_old, vec_scalar(v, h));
+}
+
+// Euler algorithm
+//results (space vectors r1, r2) in csv file
+void euler(std::vector<double> r1, std::vector<double> r2, std::vector<double> v1, std::vector<double> v2, double m1, double m2, double h, double T_max){
+    // var m1
+    std::vector<double> r1_old{0.0, 0.0};
+    std::vector<double> v1_old{0.0, 0.0};
+    std::vector<double> a1{0.0, 0.0};
+    std::vector<double> a1_old{0.0, 0.0};
+
+    // var m2
+    std::vector<double> r2_old{0.0, 0.0};
+    std::vector<double> v2_old{0.0, 0.0};
+    std::vector<double> a2{0.0, 0.0};
+    std::vector<double> a2_old{0.0, 0.0};
+
+
+    // open csv files to save the space vectors r1, r2
+    std::ofstream file_r1("build/euler_r1.csv"); 
+    std::ofstream file_r2("build/euler_r2.csv");
+
+    for(int i = 1; i*h<T_max; i++){ //t = i*h, i=1,2,..
+        //m1
+        a1_old = a1;
+        a1 = get_a(r1, r2, m2); // update a
+        
+        v1_old = v1;
+        v1 = get_v(v1_old, a1, h); //calc new velocity
+        
+        r1_old = r1;
+        r1 = get_r(r1_old, v1, h); //calc new position
+
+        file_r1 << r1[0] << ", " << r1[1] << std::endl; //write to csv
+
+        //m2
+        a2_old = a2;
+        a2 = get_a(r2, r1_old, m1); // update acceleration a
+        
+        v2_old = v2;
+        v2 = get_v(v2_old, a2, h); //calc new velocity
+        
+        r2_old = r2;
+        r2 = get_r(r2_old, v2, h); //calc new position
+        
+        file_r2 << r2[0] << ", " << r2[1] << std::endl; //write to csv        
+    }
+
+    // close csv files
+    file_r1.close();
+    file_r2.close();
+}
+
+// Verlet algorithm
+void verlet(std::vector<double> r1, std::vector<double> r2, double m1, double m2, double h, double T_max){
+    // var m1
+    std::vector<double> r1_old{0.0, 0.0}; // r_n
+    std::vector<double> r1_oold{0.0, 0.0}; // r_n-1
+
+    std::vector<double> a1{0.0, 0.0};
+    std::vector<double> a1_old{0.0, 0.0};
+
+    // var m2
+    std::vector<double> r2_old{0.0, 0.0};
+    std::vector<double> r2_oold{0.0, 0.0};
+
+    std::vector<double> a2{0.0, 0.0};
+    std::vector<double> a2_old{0.0, 0.0}; 
+
+
+    // open csv files to save the space vectors r1, r2
+    std::ofstream file_r1("build/verlet_r1.csv"); 
+    std::ofstream file_r2("build/verlet_r2.csv");
+
+    for(int i = 1; i*h<T_max; i++){ //t = i*h, i=1,2,..
+        //m1
+        a1_old = a1;
+        a1 = get_a(r1, r2, m2); // update a
+
+        r1_oold = r1_old;
+        r1_old = r1;
+
+        // r_n+1 = 2*r_n - r_n-1 + a_n*h^2
+        r1 = vec_add(vec_sub(vec_scalar(r1_old, 2), r1_oold), vec_scalar(a1, h*h));
+
+        file_r1 << r1[0] << ", " << r1[1] << std::endl; //write to csv
+
+        //m2
+        a2_old = a2;
+        a2 = get_a(r2, r1, m1); // update a
+
+        r2_oold = r2_old;
+        r2_old = r2;
+
+        // r_n+1 = 2*r_n - r_n-1 + a_n*h^2
+        r2 = vec_add(vec_sub(vec_scalar(r2_old, 2), r2_oold), vec_scalar(a2, h*h));
+
+        file_r2 << r2[0] << ", " << r2[1] << std::endl; //write to csv
+    }
+
+    // close csv files
+    file_r1.close();
+    file_r2.close();
+}
+
+int main(){
+    // Var mass 1: initial conditions
+    const double m1 = 1.0;
+    const std::vector<double> r1{0.0, 1.0};
+    const std::vector<double> v1{0.8, 0.0};
+
+    // Var mass 2: initial conditions
+    const double m2 = 2.0;
+    const std::vector<double> r2{0.0, -0.5};
+    const std::vector<double> v2{-0.4, 0.0};
+
+    // Var time
+    // t = i*h = 1*h, 2*h, ..., T_max
+    const double h = 0.01;
+    const double T_max = 100.0;
+
+    // Euler algorithm
+    euler(r1, r2, v1, v2, m1, m2, h, T_max);
+
+    // Verlet algorithm
+    // verlet(r1, r2, m1, m2, g, T_max);
+
+
+    return 0;
+}
