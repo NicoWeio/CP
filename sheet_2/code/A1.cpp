aa34025c4c6f2a6bedfd6b964b2118adc105a7df
Slin1999 <selin88.sz@gmail.com>
2023-04-26 17:47:21 +0200

    sheet 2 a1 zwischenstand

--- sheet_2/code/A1.cpp
+++ sheet_2/code/A1.cpp
@@ -2,11 +2,32 @@
 #include <functional>
 #include <iostream>
 #include <math.h>
+#include <eigen3/Eigen/Dense>
+using namespace std;
 
-double f2(double x) {
-    return x * sin(1 / x);
+Eigen::Vector3d RungeKutter(double r0, double z0, double phi0, double h){
+    Eigen::Vector3d y(r0,z0,phi0);
+    Eigen::Vector3d k1= df(y);
+    Eigen::Vector3d k2 = df ( y + h/2*k1);
+    Eigen::Vector3d k3 = df ( y + h/2*k2);
+    Eigen::Vector3d k3 = df ( y + h*k3);
+    return y + h * ()
+
+
+
+
+    return y1;
 }
 
+
+Eigen::Vector3d df(Eigen::Vector3d y){
+    Eigen::Vector3d dy(cos(y(2)), sin(y(2)), 2 - 0.1* y(1)- sin(y(2))/y(0));
+    return dy;
+}
+
+
 int main() {
-    return 0;
+    Eigen::Vector3d y(1,1,0);
+   cout<< df(y);
+  return 0;
 }
