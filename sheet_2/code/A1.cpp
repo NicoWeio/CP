4d844d451f196c380ad1d02ccfe65265ab9783ee
Slin1999 <selin88.sz@gmail.com>
2023-04-26 19:58:51 +0200

    bla

--- sheet_2/code/A1.cpp
+++ sheet_2/code/A1.cpp
@@ -5,29 +5,41 @@
 #include <eigen3/Eigen/Dense>
 using namespace std;
 
-Eigen::Vector3d RungeKutter(double r0, double z0, double phi0, double h){
-    Eigen::Vector3d y(r0,z0,phi0);
+Eigen::Vector3d df(Eigen::Vector3d y){
+    Eigen::Vector3d dy;
+    if(y(0)==0){
+        dy(0) = cos(y(2));
+        dy(1) = sin(y(2));
+        dy(2) = (2 - 0.1* y(1))/2;
+    }
+    else{
+        dy(0) = cos(y(2));
+        dy(1) = sin(y(2));
+        dy(2) = 2 - 0.1* y(1)- sin(y(2))/y(0);
+    }
+    return dy;
+}
+
+Eigen::Vector3d RungeKutter(Eigen::Vector3d y0, double h){
+    Eigen::Vector3d y = y0;
     Eigen::Vector3d k1= df(y);
     Eigen::Vector3d k2 = df ( y + h/2*k1);
     Eigen::Vector3d k3 = df ( y + h/2*k2);
-    Eigen::Vector3d k3 = df ( y + h*k3);
-    return y + h * ()
-
-
-
-
-    return y1;
-}
-
-
-Eigen::Vector3d df(Eigen::Vector3d y){
-    Eigen::Vector3d dy(cos(y(2)), sin(y(2)), 2 - 0.1* y(1)- sin(y(2))/y(0));
-    return dy;
+    Eigen::Vector3d k4 = df ( y + h*k3);
+    return y + h * (k1 + 2*k2+2*k3 + k4)/6;
 }
 
 
 int main() {
-    Eigen::Vector3d y(1,1,0);
-   cout<< df(y);
+    double h =0.01;
+    Eigen::Vector3d y(0,0,0);
+    Eigen::Vector3d yn;
+    ofstream File("build/A1.csv");
+    for(double i=0; i< 1000; i+=h){
+    yn=RungeKutter(y, h);
+    File << i*h << "," << yn(0)<< ","<< yn(1)<< "," << yn(2)<< endl;
+    y = yn;
+    }
+    File.close();
   return 0;
 }
