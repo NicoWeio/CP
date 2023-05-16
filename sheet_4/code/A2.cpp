e68d370efc898833a359154aef75d665071232a7
SamGTex <samuel.haefs@tu-dortmund.de>
2023-05-16 18:38:24 +0200

    A2 b) done, make python file more general and plotting results of a) and b) now

--- sheet_4/code/A2.cpp
+++ sheet_4/code/A2.cpp
@@ -18,37 +18,54 @@ class Diffusion {
         double *u;
 
         // methods
-        Diffusion(double D, double L, string initial);
-        void solve(double dt, double dx, uint t_steps, string path_write);
-        void set_inital_conditions();
+        Diffusion(double D, double L, double dx);
+        void solve(double dt, uint t_steps, string path_write);
+
+        void set_initial_const(double a); // a) set initial condition to constant 
+        void set_initial_delta(double x0, double a); // b) set initial condition to delta function with height "a"
 };
 
 // constructor
-Diffusion::Diffusion(double D, double L, string initial) {
+Diffusion::Diffusion(double D, double L, double dx) {
+    // set parameters
     this->D = D;
     this->L = L;
-    
+    this->dx = dx;
+    this->x_steps = uint(L/dx);
 }
 
 // set inital conditions
-void Diffusion::set_inital_conditions(){
-    // initial condition
+void Diffusion::set_initial_const(double a){
+    // u_0 = a 
     u = new double[x_steps+1];
 
     for (uint i=0; i<=x_steps; i++) {
-        u[i] = 1;
+        u[i] = a;
+    }
+}
+
+void Diffusion::set_initial_delta(double x0, double a){
+    // u_0 = delta(x-a) with height a
+    u = new double[x_steps+1];
+    
+    // get bin position of x0
+    uint pos = uint(x0/dx);
+
+    for (uint i=0; i<=x_steps; i++){
+        if (i==pos){
+            u[i] = a;
+        }
+        else{
+            u[i] = 0.0;
+        }
     }
 }
 
 // solve the diffusion equation
-void Diffusion::solve(double dt, double dx, uint t_steps, string path_write) {
+void Diffusion::solve(double dt, uint t_steps, string path_write) {
     // set parameters
-    this->dx = dx;
     this->dt = dt;
     this->t_steps = t_steps;
-    this->x_steps = uint(L/dx);
-
-    set_inital_conditions();
 
     // constant
     double C = D*dt/(dx*dx);
@@ -81,18 +98,37 @@ void Diffusion::solve(double dt, double dx, uint t_steps, string path_write) {
     outfile.close();
 }
 
+
 int main() {
     double D = 1;
     const double L = 1.0;
     double dx = 0.01;
-    string path = "build/A1.csv";
     
+    // a)
     double dt = 0.00001;
     uint t_steps = 40000;
     
+    Diffusion dif_a(D, L, dx);
+    dif_a.set_initial_const(1);
+    dif_a.solve(dt, t_steps, "build/A2_a).csv");
+    
 
-    Diffusion dif(D, L, "step");
-    dif.solve(dt, dx, t_steps, path);
+    // b)
+    // dt fullfills stability condition -> stable
+    dt = 0.00001;
+    t_steps = 300;
+
+    Diffusion dif_b_slow(D, L, dx);
+    dif_b_slow.set_initial_delta(0.5, 1);
+    dif_b_slow.solve(dt, t_steps, "build/A2_b)slow.csv");
+
+    // dt too large -> unstable
+    dt = 0.0001;
+    t_steps = 300;
+
+    Diffusion dif_b_fast(D, L, dx);
+    dif_b_fast.set_initial_delta(0.5, 1);
+    dif_b_fast.solve(dt, t_steps, "build/A2_b)fast.csv");
 
     
     return 0;
--- sheet_4/code/plot_A2.py
+++ sheet_4/code/plot_A2.py
@@ -2,45 +2,63 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-#Parameters
-n = 40 # hold every n-th frame
+def animate(df_data, n):
+    # hold every n-th frame
+    df = df_data.iloc[::n, :].copy()
 
-# Read data
-df = pd.read_csv("build/A1.csv", header=None)
+    # remove last column (line ends with ,)
+    t = df.iloc[:, 0].to_numpy()
+    df.drop(df.columns[[0]], axis=1, inplace=True)
 
-# hold every n-th frame
-df = df.iloc[::n, :]
+    # print total frames
+    print("Total frames: ", len(df))
 
-# remove last column (line ends with ,)
-t = df.iloc[:, 0].to_numpy()
-df.drop(df.columns[[0]], axis=1, inplace=True)
+    # create FuncAnimation to display the one dimensional diffusion equation
+    fig, ax = plt.subplots()
 
-# print total frames
-print("Total frames: ", len(df))
+    # set axis limits
+    ax.set_xlim(0, len(df.columns)-1)
+    ax.set_ylim(df.min().min(), df.max().max()+0.01)
 
-# create FuncAnimation to display the one dimensional diffusion equation
-fig, ax = plt.subplots()
+    ax.set_xlabel("x")
+    ax.set_ylabel("u(x,t)")
 
-# set axis limits
-ax.set_xlim(0, len(df.columns)-1)
-ax.set_ylim(df.min().min(), df.max().max()+0.01)
+    # plot initial condition
+    line, = ax.plot(df.iloc[0, :], color="black")
 
-ax.set_xlabel("x")
-ax.set_ylabel("u(x,t)")
+    # update function
+    def update(frame):
+        line.set_ydata(df.iloc[frame, :])
+        
+        if frame % 10 == 0:
+            ax.set_title("Time = " + str(t[frame].round(2)))
+        return line, ax
 
-# plot initial condition
-line, = ax.plot(df.iloc[0, :], color="black")
+    # create animation
+    anim = FuncAnimation(fig, update, frames=len(df), interval=1)
+    return anim
 
-# update function
-def update(frame):
-    line.set_ydata(df.iloc[frame, :])
-    
-    if frame % 10 == 0:
-        ax.set_title("Time = " + str(t[frame].round(2)))
-    return line, ax
+if __name__=="__main__":
+    # time of animation = len(df)/(n*fps)
 
-# create animation
-anim = FuncAnimation(fig, update, frames=len(df), interval=1)
-plt.show()
-# save animation
-anim.save("build/A2.mp4", dpi=300, fps=100)
+    # a)
+    print("\nPlot a)")
+    df_a = pd.read_csv("build/A2_a).csv", header=None)
+    animation = animate(df_a, n=40)
+    plt.show()
+    #animation.save("build/A2_a).mp4", dpi=300, fps=100)
+
+    # b)
+    # stable
+    print("\nPlot b) small dt")
+    df_b_slow = pd.read_csv("build/A2_b)slow.csv", header=None)
+    animation = animate(df_b_slow, n=1)
+    plt.show()
+    #animation.save("build/A2_b)slow.mp4", dpi=300, fps=30)
+
+    #unstable
+    print("\nPlot b) large dt")
+    df_b_fast = pd.read_csv("build/A2_b)fast.csv", header=None)
+    animation = animate(df_b_fast, n=1)
+    plt.show()
+    #animation.save("build/A2_b)fast.mp4", dpi=300, fps=30)
