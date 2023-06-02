8c1d71e90f95e629ef70ffe623c49ae8166012bb
SamGTex <samuel.haefs@tu-dortmund.de>
2023-06-02 11:29:51 +0200

    start latex

--- sheet_6/code/A1.py
+++ sheet_6/code/A1.py
@@ -8,6 +8,12 @@ def map_log(N: int, x0: float, r: float):
 
     return x0
 
+def map_cube(N: int, x0: float, r: float):
+    for _ in range(N):
+        x0 = r*x0 - x0**3 
+
+    return x0
+
 if __name__ == "__main__":
     # set values
     # r space
@@ -27,40 +33,55 @@ if __name__ == "__main__":
     # create array with r values
     r_space = np.arange(min_r, max_r, spacing)
     print(f'Shape of r: {r_space.shape}')
-
-    # create array with initial values x0 including 1
-    x0_space = np.arange(0.0, 1.0+dx, dx)
-
-    # create empty array for x
-    x_space = np.zeros((len(r_space), len(x0_space)))
-    print(f'Shape of full x array: {x_space.shape}')
     
+    x0_space = np.arange(0.0, 1.0+dx, dx) #create array with initial values x0 including 1
+
     # create empty array for x
-    x_space = np.zeros((len(r_space), len(x0_space)))
-    print(f'Shape of full x array: {x_space.shape}')
+    x_space_log = np.zeros((len(r_space), len(x0_space)))
+    x_space_cube = np.zeros((len(r_space), len(x0_space)))
+
+    print(f'Shape of full x array: {x_space_log.shape}')
 
     # calc logistic map for each x0 and r
     for ind, x0 in enumerate(x0_space):
-        x_space[:, ind] = map_log(N, x0, r_space)  # vectorized calculation
+        x_space_log[:, ind] = map_log(N, x0, r_space)  # vectorized calculation
+        x_space_cube[:, ind] = map_cube(N, x0, r_space)  # vectorized calculation
 
     # remove data points: x < 0 or x > 1
-    x_space[(x_space < 1E-8) | (x_space > 1.0)] = np.nan
+    x_space_log[(x_space_log < 1E-8) | (x_space_log > 1.0)] = np.nan
+    x_space_cube[(x_space_cube < 1E-8) | (x_space_cube > 1.0)] = np.nan
     
 
     # TODO: calc Feigenbaum constant
     
 
-    # plot
+    # plot logistic map
     plt.figure(figsize=(10,8), dpi=200)
 
     for ind, x0 in enumerate(x0_space):
         label_name = f'x0 = {x0:.1f}' if ind % 10 == 0 else None
-        plt.plot(r_space, x_space[:, ind], '.', linewidth=0.2, markersize=0.2, label=label_name, color=f'C{ind}')
+        plt.plot(r_space, x_space_log[:, ind], '.', linewidth=0.2, markersize=0.2, label=label_name, color=f'C{ind}')
 
     #plt.legend(fontsize=5, loc='upper left')
     plt.xlabel('r')
     plt.ylabel('x')
-    plt.title('Bifurcation diagram')
+    plt.title('Bifurcation diagram (logistic map)')
     plt.legend(fontsize=10, loc='upper left')
-    plt.savefig("build/bifurkation_log.pdf")
+    plt.savefig("build/A1_bifurkation_log.pdf")
+    plt.show()
+
+
+    # plot logistic map
+    plt.figure(figsize=(10,8), dpi=200)
+
+    for ind, x0 in enumerate(x0_space):
+        label_name = f'x0 = {x0:.1f}' if ind % 10 == 0 else None
+        plt.plot(r_space, x_space_cube[:, ind], '.', linewidth=0.2, markersize=0.2, label=label_name, color=f'C{ind}')
+
+    #plt.legend(fontsize=5, loc='upper left')
+    plt.xlabel('r')
+    plt.ylabel('x')
+    plt.title('Bifurcation diagram (cubic map)')
+    plt.legend(fontsize=10, loc='upper left')
+    plt.savefig("build/A1_bifurkation_cube.pdf")
     plt.show()
